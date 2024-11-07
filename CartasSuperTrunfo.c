627b4d0b289f1d4260f603a041ed7d2b503b28d7
Lion60 <liondeneve@gmail.com>
2024-11-07 14:08:03 +0000

    Inseri o modo aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@
 
 int main() {
 
-    printf ("***Desafio das cartas - novato*** \n");
+    printf ("***Desafio das cartas - Novato e Aventureiro*** \n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
@@ -20,13 +20,20 @@ int main() {
     int pib = 0;
     int pontosturisticos = 0;
 
+    //Declaração novas variáveis modo Aventureiro:
+
+    float densid = 0.0;
+    float PIBcapita = 0.0;
+
     // Entrada dados
 
     printf ("Digite o código da Cidade: \n");
-    scanf ("%s", &codcidade);
-
+    scanf ("%s" , &codcidade);
+               
+    
     printf ("Digite o nome da Cidade: \n");
     scanf ("%s", &nomecidade);
+
 
     printf ("Digite a População: \n");
     scanf ("%d", &populacao);
@@ -39,6 +46,11 @@ int main() {
 
     printf ("O número de pontos turísticos: \n");
     scanf ("%d", &pontosturisticos);
+
+    // Função Densidade Populacional e PIB per capita *** Aventureiro***
+
+    densid = populacao / area;
+    PIBcapita = (float) pib / populacao; // Casting 
 
      // Mostrando dados capturados: 
 
@@ -53,6 +65,10 @@ int main() {
     printf ("O PIB da Cidade é: %d\n", pib);
 
     printf ("O número de pontos turísticos da Cidade é: %d\n", pontosturisticos);
+
+    printf("A densidade populacional é: %.2f \n ", densid);
+
+    printf("O PIB per capita é: %.4f \n ", PIBcapita );
        
     return 0;
 }
